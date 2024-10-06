b7c1a69869f6f309a5caa6091e5832623cdc81c3
Ueli Kunz <kunz@ideadapt.net>
2024-10-06 23:59:57 +0200

    PoC webhook based sync.

--- receipt-summary-server/src/main/kotlin/net/ideadapt/plugins/Routing.kt
+++ receipt-summary-server/src/main/kotlin/net/ideadapt/plugins/Routing.kt
@@ -17,25 +17,93 @@ import com.aallam.openai.api.vectorstore.VectorStore
 import com.aallam.openai.api.vectorstore.VectorStoreRequest
 import com.aallam.openai.client.OpenAI
 import com.aallam.openai.client.OpenAIConfig
+import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
+import org.intellij.lang.annotations.Language
 
-//import io.ktor.server.sse.*
-//import io.ktor.sse.*
+@Serializable
+data class FileNode(
+    val internalPath: String,
+    val modifiedTime: Long,
+    val mimeType: String,
+    val size: Long,
+)
+
+@Language("JSON")
+val x = """ 
+{
+  "eventType": "OCA\\WorkflowEngine\\Entity\\File",
+  "eventName": "\\OCP\\Files::postCreate",
+  "node": {
+    "id": 249291,
+    "storage": {
+      "cache": null,
+      "scanner": {},
+      "watcher": null,
+      "propagator": null,
+      "updater": {}
+    },
+    "path": "\/uk-phone\/files\/receipts\/Receipt_20240718_151446_0090466_003_347.pdf",
+    "internalPath": "files\/receipts\/Receipt_20240718_151446_0090466_003_347.pdf",
+    "modifiedTime": 1727524714,
+    "mimeType": "application\/pdf",
+    "size": 129191,
+    "Etag": "fd0cbab8e54363c9de8567bbe924c35f",
+    "permissions": 27,
+    "isUpdateable": true,
+    "isDeletable": true,
+    "isShareable": true
+  },
+  "workflowFile": {
+    "displayText": "... created Receipt_20240718_151446_0090466_003_347.pdf",
+    "url": "https:\/\/ideadapt.net\/nextcloud\/index.php\/f\/249291"
+  }
+}
+"""
+
+@Serializable
+data class FlowEventFileCreate(
+    val eventName: String,
+    val node: FileNode,
+)
 
 @OptIn(BetaOpenAI::class)
 fun Application.configureRouting() {
-    //install(SSE)
+
     routing {
-        get("/gists/{gist_id}") {
-            val gistId = call.parameters["gist_id"]
-            call.respondText("Hello $gistId")
+        post("/hooks") {
+            // TODO https://github.com/kffl/nextcloud-webhooks/blob/master/README.md#authenticating-requests
+            val body = call.receiveText()
+            call.application.environment.log.info("Received hook {}", body)
+            if (body.contains("\\OCP\\Files::postCreate")) {
+                val event = call.receive(FlowEventFileCreate::class)
+
+                launch(Dispatchers.Default) { // run in dedicated thread pool, pool size = nr of CPUs
+                    // TODO process
+                    //  val job = job.createOrGet(event)
+                    //  if(job.status == NEW)
+                    //      val buffer = nx.readFile(event)
+                    //      val csv = ai.analyze(buffer)
+                    //      val status = receipts.merge(csv) // {merged: 2, new: 2, total: 36, filename: 'ejeje.pdf'}
+                    //      log(status)
+                    //      job.done(jobId)
+                    println(event)
+                }
+
+                call.respond(HttpStatusCode.Accepted)
+            } else {
+                call.respond(HttpStatusCode.NotImplemented)
+            }
         }
+
         post("/receipts") {
             val token = requireNotNull(System.getenv("OPEN_AI_TOKEN")) { "OPEN_AI_TOKEN missing" }
             val ai = OpenAI(config = OpenAIConfig(token = token))
--- receipt-summary-server/src/main/kotlin/net/ideadapt/plugins/Serialization.kt
+++ receipt-summary-server/src/main/kotlin/net/ideadapt/plugins/Serialization.kt
@@ -3,16 +3,12 @@ package net.ideadapt.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
-    }
-    routing {
-        get("/json/kotlinx-serialization") {
-            call.respond(mapOf("hello" to "wwworld"))
-        }
+        json(json = Json {
+            ignoreUnknownKeys = true
+        })
     }
 }
